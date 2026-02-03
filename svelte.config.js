8d0dc6c6144c9a3aa8dc0e47e15e4c339c153ab0
Alman_CyberSec <manuelgrandeecija1@gmail.com>
2026-02-03 02:07:51 +0100

    Update svelte.config.js with adapter-static configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 export default {
   preprocess: preprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: dev ? '' : '/Portafolio-Profesional'
+    },
+    prerender: {
+      default: true
+    }
   }
 };
