0c13ecc9eee02cafdffb360077cc3752ace128c2
Manuel Grande <manuelgrandeecija1@gmail.com>
2026-02-03 22:28:07 +0100

    fix: eliminar +page.server.js incompatible

--- src/routes/projects/[slug]/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { projects } from '$lib/data/projects';
-
-export const prerender = true;
-
-export function entries() {
-    return projects.map((project) => ({
-        slug: project.slug
-    }));
-}
